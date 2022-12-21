eaa9c7d95b6907787147f4c59f5a45b6a957940b
Jachdich <jamesedkitching@gmail.com>
2022-12-21 13:11:48 +0000

    make shared not panic in case of a weird error. maybe gota fix that properly later

--- src/shared.rs
+++ src/shared.rs
@@ -38,7 +38,7 @@ impl Shared {
     pub fn send_to_all(&self, message: MessageType) {
         for peer in self.peers.iter() {
             if let Err(e) = peer.tx.send(message.clone()) {
-                panic!("Fatal(Shared::send_to_all): I think this is unlikely but `peer.tx.send` failed. idk bug me to fix it ig. {:?}", e);
+                println!("Error(Shared::send_to_all): I think this is unlikely but `peer.tx.send` failed. idk bug me to fix it ig. {:?}", e);
             }
         }
     }
