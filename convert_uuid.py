0551816a9b4eaaa72270e189126a1c795f52f516
Jachdich <jek@cospox.com>
2024-06-27 22:50:32 +0100

    utility to convert the uuids to no more than 2**53-1 in size (fucking javascript)

---
+++ convert_uuid.py
@@ -0,0 +1,36 @@
+import sqlite3
+import shutil
+
+shutil.copy("aster.db", "aster_backup_uuidconversion.db")
+
+con = sqlite3.connect("aster.db")
+cur = con.cursor()
+for table in ["messages", "channels", "users", "emojis", "sync_servers"]:
+    for row in cur.execute(f"SELECT uuid FROM {table}"):
+        uuid, = row
+        if uuid > 2**53-1:
+            new_uuid = uuid >> (63 - 53)
+            cur.execute(f"update {table} set uuid={new_uuid} where uuid={uuid}")
+
+for row in cur.execute("SELECT uuid,author_uuid,channel_uuid FROM messages"):
+    uuid, author_uuid, channel_uuid = row
+    if author_uuid > 2**53-1:
+        author_uuid = author_uuid >> (63 - 53)
+    if channel_uuid > 2**53-1:
+        channel_uuid = channel_uuid >> (63 - 53)
+
+    cur.execute(f"update messages set author_uuid={author_uuid}, channel_uuid={channel_uuid} where uuid={uuid}")
+
+for row in cur.execute("SELECT user_uuid FROM sync_data"):
+    uuid, = row
+    if uuid > 2**53-1:
+        new_uuid = uuid >> (63 - 53)
+        cur.execute(f"update sync_data set user_uuid={new_uuid} where user_uuid={uuid}")
+
+for row in cur.execute("SELECT user_uuid FROM sync_servers"):
+    uuid, = row
+    if uuid > 2**53-1:
+        new_uuid = uuid >> (63 - 53)
+        cur.execute(f"update sync_servers set user_uuid={new_uuid} where user_uuid={uuid}")
+
+con.commit()
