ec76e338a5a17cbc671ce0d79da2cddfacfca055
Jachdich <jamesedkitching@gmail.com>
2025-05-06 00:57:47 +0100

    idk init2??

---
+++ init2.sql
@@ -0,0 +1,55 @@
+CREATE TABLE channels (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL
+);
+CREATE TABLE messages (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    content text NOT NULL,
+    author_uuid BigInt NOT NULL,
+    channel_uuid BigInt NOT NULL,
+    date integer NOT NULL, edited Integer not null default 0, reply BigInt,
+    FOREIGN KEY (author_uuid) REFERENCES users(uuid),
+    FOREIGN KEY (channel_uuid) REFERENCES channels(uuid)
+);
+CREATE TABLE users (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL,
+    pfp text NOT NULL,
+    group_uuid BigInt NOT NULL, password Text not null default "",
+    FOREIGN KEY (group_uuid) REFERENCES groups(uuid)
+);
+CREATE TABLE groups (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    permissions BigInt NOT NULL,
+    name text NOT NULL,
+    colour integer NOT NULL
+);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE user_groups (
+    link_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_uuid BigInt NOT NULL,
+    group_uuid BigInt NOT NULL,
+    FOREIGN KEY (user_uuid) REFERENCES users(uuid),
+    FOREIGN KEY (group_uuid) REFERENCES groups(uuid)
+);
+CREATE TABLE sync_data (
+    user_uuid BigInt PRIMARY KEY NOT NULL,
+    uname text NOT NULL,
+    pfp text NOT NULL
+);
+CREATE TABLE emojis (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL,
+    data text nOT NULL
+);
+CREATE TABLE sync_servers (
+    user_uuid BigInt NOT NULL,
+    uuid BigInt,
+    uname Text NOT NULL,
+    ip Text NOT NULL,
+    port Integer NOT NULL,
+    pfp Text,
+    name Text,
+    idx Integer NOT NULL,
+    rowid Integer NOT NULL PRIMARY KEY
+);
