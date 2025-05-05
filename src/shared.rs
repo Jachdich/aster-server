6610ad60b173914581e601d5bd336fbee1f6a443
Jachdich <jek@cospox.com>
2025-05-05 15:42:55 +0100

    begin to create tables if not exists

--- src/shared.rs
+++ src/shared.rs
@@ -22,7 +22,7 @@ type DbError = rusqlite::Error;
 
 impl Shared {
     pub fn new() -> Self {
-        todo!("Check the schema!!");
+        // todo!("Check the schema!!");
         // let sqlitedb = SqliteConnection::establish(&CONF.database_file).unwrap_or_else(|_| {
         //     panic!(
         //         "Fatal(Shared::new) connecting to the database file {}",
@@ -30,6 +30,67 @@ impl Shared {
         //     )
         // });
         let sqlitedb = Connection::open_in_memory().expect("Unable to create a DB?");
+
+        sqlitedb.execute_batch(r#"
+BEGIN;
+CREATE TABLE channels (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL
+);
+CREATE TABLE messages (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    content text NOT NULL,
+    author_uuid BigInt NOT NULL,
+    channel_uuid BigInt NOT NULL,
+    date integer NOT NULL,
+    edited Integer not null default 0,
+    reply BigInt,
+    FOREIGN KEY (author_uuid) REFERENCES users(uuid),
+    FOREIGN KEY (channel_uuid) REFERENCES channels(uuid)
+);
+CREATE TABLE users (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL,
+    pfp text NOT NULL,
+    group_uuid BigInt NOT NULL,
+    password text NOT NULL,
+    FOREIGN KEY (group_uuid) REFERENCES groups(uuid)
+);
+CREATE TABLE groups (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    permissions BigInt NOT NULL,
+    name text NOT NULL,
+    colour integer NOT NULL
+);
+CREATE TABLE user_groups (
+    link_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_uuid BigInt NOT NULL,
+    group_uuid BigInt NOT NULL,
+    FOREIGN KEY (user_uuid) REFERENCES users(uuid),
+    FOREIGN KEY (group_uuid) REFERENCES groups(uuid)
+);
+CREATE TABLE sync_data (
+    user_uuid BigInt PRIMARY KEY NOT NULL,
+    uname text NOT NULL,
+    pfp text NOT NULL
+);
+CREATE TABLE emojis (
+    uuid BigInt PRIMARY KEY NOT NULL,
+    name text NOT NULL,
+    data text NOT NULL
+);
+CREATE TABLE sync_servers (
+    user_uuid BigInt NOT NULL,
+    uuid BigInt,
+    uname Text NOT NULL,
+    ip Text NOT NULL,
+    port Integer NOT NULL,
+    pfp Text,
+    name Text,
+    idx Integer NOT NULL,
+    rowid Integer NOT NULL PRIMARY KEY
+);
+COMMIT;"#).unwrap();
 
         Shared {
             online: HashMap::new(),
